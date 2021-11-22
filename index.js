380d25c3cb93aa055e4154a3e9ac0b47f04bd6f2
Sergiy <sdudnik@ukr.net>
2021-11-22 07:13:12 +0200

    test0410

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ const start = () => {
                 if (result) {
                     console.log(`Found a listing in the collection with the name '${nameOfListing}'`);
                     console.log(result);
-                    return bot.sendMessage(chatId, result)
+                    return bot.sendMessage(chatId, 'Hello from mongo' + msg.result)
                 } else {
                     console.log(`No listing found with the name '${nameOfListing}'`);
                 }
@@ -78,7 +78,7 @@ const start = () => {
             }
 // end of connect to mongodb database
 
-            return bot.sendMessage(chatId,'Тебе звати+3 ' + msg.from.first_name )
+            return bot.sendMessage(chatId,'Тебе звати+4 ' + msg.from.first_name)
             //    return bot.sendMessage(chatId, 'Databases: ')
         }
 //
